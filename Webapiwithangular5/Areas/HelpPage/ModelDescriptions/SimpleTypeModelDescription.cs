83487b65610bfc84d77e44d0875e2e7f3d7a04b3
sampanna pokhrel <sampanna99@gmail.com>
2018-03-10 19:35:39 -0500

    Add project files.

---
+++ Webapiwithangular5/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Webapiwithangular5.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Webapiwithangular5/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Webapiwithangular5.Startup))]
+
+namespace Webapiwithangular5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
